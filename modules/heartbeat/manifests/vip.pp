d2a284fb82767373435f9f96deb5c592e125a0de
jigc <mrji1990@gmail.com>
2015-05-28 20:56:38 +0800

    其他示例

---
+++ modules/heartbeat/manifests/vip.pp
@@ -0,0 +1,16 @@
+# Manage a specific VIP with Heartbeat
+class
+heartbeat::vip($node1,$node2,$ip1,$ip2,$vip,$interface='eth0:1')
+{
+	include heartbeat
+	file { '/etc/ha.d/haresources':
+		content => "${node1} IPaddr::${vip}/${interface}\n",
+		notify => Exec['reload-heartbeat'],
+		require => Package['heartbeat'],
+	}
+	file { '/etc/ha.d/ha.cf':
+		content => template('heartbeat/vip.ha.cf.erb'),
+		notify => Exec['reload-heartbeat'],
+		require => Package['heartbeat'],
+	}
+}
